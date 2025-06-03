70ee484892e84ac6e9ded58377caa7e913539d82
ajaypalamkunnel <ajaypalamkunnel45@gmail.com>
2025-06-03 16:17:57 +0530

    socket connection secured

--- src/index.ts
+++ src/index.ts
@@ -1,58 +1,61 @@
-import express from 'express';
-import dotenv from 'dotenv';
-dotenv.config()
-import http from 'http'
-import cookieParser from 'cookie-parser'
-import bodyParser from 'body-parser';
-import cors from 'cors'
-import connectDB from './config/dbConfig';
-import userRouter from './routes/user/userRoutes'
-import adminRouter from './routes/admin/adminRoutes'
-import chatRouter from './routes/chat/chatRoutes'
-import doctorRouter from './routes/doctor/doctorRoutes'
-import session from 'express-session'
-import passport from 'passport'
+import express from "express";
+import dotenv from "dotenv";
+dotenv.config();
+import http from "http";
+import cookieParser from "cookie-parser";
+import bodyParser from "body-parser";
+import cors from "cors";
+import connectDB from "./config/dbConfig";
+import userRouter from "./routes/user/userRoutes";
+import adminRouter from "./routes/admin/adminRoutes";
+import chatRouter from "./routes/chat/chatRoutes";
+import doctorRouter from "./routes/doctor/doctorRoutes";
+import session from "express-session";
+import passport from "passport";
 import { Server as SocketIOServer } from "socket.io";
-import "./config/passport"
-import morganMiddleware from './middleware/morganMiddleware';
-import agoraRouter from './routes/agora/agoraTokenRoute';
-import { startPendingSlotCleanupJob } from './jobs/pendingSlotCleanup';
-import { initializeSocket } from './utils/chatSocket';
+import "./config/passport";
+import morganMiddleware from "./middleware/morganMiddleware";
+import agoraRouter from "./routes/agora/agoraTokenRoute";
+import { startPendingSlotCleanupJob } from "./jobs/pendingSlotCleanup";
+import { initializeSocket } from "./utils/chatSocket";
+import sharedsession from "express-socket.io-session";
 
-connectDB()
-const app = express()
-const server = http.createServer(app)
-const PORT = process.env.PORT || 5000
+connectDB();
+const app = express();
+const server = http.createServer(app);
+const PORT = process.env.PORT || 5000;
+
+// Session
+
+const sessionMiddleWare = session({
+    secret: "wellcare",
+    resave: false,
+    saveUninitialized: false,
+    cookie: { secure: false, httpOnly: true },
+});
 
 // Middleware
-app.use(cookieParser())
-app.use(express.json({ limit: '100mb' }))
-app.use(express.urlencoded({ limit: '100mb', extended: true }))
-app.use(bodyParser.json())
+app.use(cookieParser());
+app.use(express.json({ limit: "100mb" }));
+app.use(express.urlencoded({ limit: "100mb", extended: true }));
+app.use(bodyParser.json());
 app.use(morganMiddleware);
 
 // CORS
-app.use(cors({
-    origin: "http://localhost:3000",
-    credentials: true,
-    methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization"],
-    exposedHeaders: ["set-cookie"],
-}))
-
-// Session
 app.use(
-    session({
-        secret: "wellcare",
-        resave: false,
-        saveUninitialized: false,
-        cookie: { secure: false, httpOnly: true }
+    cors({
+        origin: "http://localhost:3000",
+        credentials: true,
+        methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
+        allowedHeaders: ["Content-Type", "Authorization"],
+        exposedHeaders: ["set-cookie"],
     })
-)
+);
 
 // Passport
-app.use(passport.initialize())
-app.use(passport.session())
+app.use(sessionMiddleWare);
+app.use(passport.initialize());
+app.use(passport.session());
 
 // Socket.io
 export const io = new SocketIOServer(server, {
@@ -60,25 +63,32 @@ export const io = new SocketIOServer(server, {
         origin: "http://localhost:3000",
         credentials: true,
         methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
-        allowedHeaders: ["Content-Type", "Authorization"]
-    }
-})
+        allowedHeaders: ["Content-Type", "Authorization"],
+    },
+});
 
-initializeSocket(io)
+// Attach session to socket
 
+io.use(
+    sharedsession(sessionMiddleWare, {
+        autoSave: true,
+    })
+);
+
+initializeSocket(io);
 
 // Routes
-app.use("/", userRouter)
-app.use("/api/doctor", doctorRouter)
-app.use("/api/admin", adminRouter)
-app.use("/api/chat",chatRouter)
-app.use("/api/agora",agoraRouter)
-app.get('/', (req, res) => {
-    res.send("Welcome to Wellcare")
-})
+app.use("/", userRouter);
+app.use("/api/doctor", doctorRouter);
+app.use("/api/admin", adminRouter);
+app.use("/api/chat", chatRouter);
+app.use("/api/agora", agoraRouter);
+app.get("/", (req, res) => {
+    res.send("Welcome to Wellcare");
+});
 
-startPendingSlotCleanupJob().then(()=>{
+startPendingSlotCleanupJob().then(() => {
     console.log("🕒 Pending slot cleanup cron initialized");
-})
+});
 
 server.listen(PORT, () => console.log(`Server connected on port ${PORT}`));
