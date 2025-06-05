6cd756e11f6f0ce18fd66afda464e882964b0969
ajaypalamkunnel <ajaypalamkunnel45@gmail.com>
2025-06-05 12:29:24 +0530

    cors orgin changed not working in production changed to old code

--- src/index.ts
+++ src/index.ts
@@ -31,7 +31,20 @@ const PORT = process.env.PORT || 5000;
 
 app.use(
     cors({
-        origin:process.env.CORS_ORIGIN?.split(",") || [],
+        origin: function (origin, callback) {
+            console.log("CORS origin:", origin);
+
+            const allowedOrigins = [
+                "https://www.wellcare.space",
+                "https://wellcare.space",
+            ];
+
+            if (!origin || allowedOrigins.includes(origin)) {
+                callback(null, origin);
+            } else {
+                callback(new Error("Not allowed by CORS"));
+            }
+        },
         credentials: true,
         methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
         allowedHeaders: ["Content-Type", "Authorization"],
